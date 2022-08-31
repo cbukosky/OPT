270d399834561f1bb23c04b0dda04043f93efef3
Chau Van <cvan@odoo.com>
2022-08-31 14:31:37 -0700

    [FIX] opt_purchase: fix table reference in post-~drop_not_null

--- opt_purchase/migrations/15.0.1.1/post-~drop_not_null.py
+++ opt_purchase/migrations/15.0.1.1/post-~drop_not_null.py
@@ -2,5 +2,5 @@
 
 
 def migrate(cr, version):
-    cr.execute("ALTER TABLE purchase_order_line ALTER COLUMN charge_code_id DROP NOT NULL")
+    cr.execute("ALTER TABLE purchase_order ALTER COLUMN charge_code_id DROP NOT NULL")
     cr.execute("ALTER TABLE purchase_order_line ALTER COLUMN account_group_id DROP NOT NULL")
