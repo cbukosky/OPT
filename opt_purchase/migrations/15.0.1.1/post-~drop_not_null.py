6c62f36d04ea1b1174570709c744f2451a50acc5
Chau Van <cvan@odoo.com>
2022-08-31 15:16:03 -0700

    [FIX] opt_purchase: remove required=True on charge_code_id and account_group_id, removed post-~drop_not_null

--- opt_purchase/migrations/15.0.1.1/post-~drop_not_null.py
+++
@@ -1,6 +0,0 @@
-# -*- encoding: utf8 -*-
-
-
-def migrate(cr, version):
-    cr.execute("ALTER TABLE purchase_order ALTER COLUMN charge_code_id DROP NOT NULL")
-    cr.execute("ALTER TABLE purchase_order_line ALTER COLUMN account_group_id DROP NOT NULL")
