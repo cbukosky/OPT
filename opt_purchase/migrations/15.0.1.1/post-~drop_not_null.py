11e427b6804a339e376801c7970bf315b23a430d
Chau Van <cvan@odoo.com>
2022-08-31 14:23:09 -0700

    [FIX] opt_purchase: removed util import from post-~drop_not_null

--- opt_purchase/migrations/15.0.1.1/post-~drop_not_null.py
+++ opt_purchase/migrations/15.0.1.1/post-~drop_not_null.py
@@ -1,7 +1,5 @@
 # -*- encoding: utf8 -*-
 
-from odoo.addons.base.maintenance.migrations import util
-
 
 def migrate(cr, version):
     cr.execute("ALTER TABLE purchase_order_line ALTER COLUMN charge_code_id DROP NOT NULL")
