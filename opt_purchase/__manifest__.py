c6baa88d467831e0b735aa1c344c3a594a088c20
cvan-odoo <89489875+cvan-odoo@users.noreply.github.com>
2022-10-26 16:40:12 -0700

    15.0 upgrade final cvan (#65)
    * [ADD] Partially migrated opt_purchase
    
    * [IMP] Migrated purchase order model
    
    * [ADD] opt_purchase: 12.0 -> 15.0
    
    * [FIX] opt_purchase: typo on proxy_id field property
    
    * [ADD] opt_purchase: removing not-null constraint on charge_code_id, account_group_id fields to unblock upgrade
    
    * [FIX] opt_purchase: removed util import from post-~drop_not_null
    
    * [FIX] opt_purchase: fix table reference in post-~drop_not_null
    
    * [FIX] opt_purchase: remove required=True on charge_code_id and account_group_id, removed post-~drop_not_null
    
    * [ADD] opt_purchase: charge_code_id, account_group_id required in view
    
    * [FIX] opt_purchase: make account_group_id required if id is set instead
    
    * [IMP] opt_purchase: make account_group_id required where display_type is set
    
    * [FIX] opt_purchase: display_type typo
    
    * [REF] opt_purchase: clean up old code, formatting and noupdate flags
    
    Co-authored-by: Marta Taulet <mta@odoo.com>

--- opt_purchase/__manifest__.py
+++ opt_purchase/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 {
     "name": "OPT: Purchase-related customizations",
     "summary": """OPT: Purchase-related customizations""",
@@ -7,11 +6,11 @@
         - Add calculated field PO balance
     """,
     "author": "Odoo Inc",
-    "license": "OEEL-1",
+    "license": "OPL-1",
     "website": "http://www.odoo.com",
     "category": "Custom Development",
-    "version": "0.1",
-    "depends": ['purchase'],
+    "version": "1.1",
+    "depends": ['account', 'purchase'],
     "data": [
         "security/ir.model.access.csv",
         "data/mail_template.xml",
@@ -23,9 +22,8 @@
         "views/purchase_level_views.xml",
         "views/purchase_proxy_views.xml",
         "views/purchase_charge_code_views.xml",
-        "views/account_invoice_views.xml",
+        "views/account_move_views.xml",
         "wizard/wizard_import_helper.xml",
-        # "wizard/wizard_purchase_levels.xml",
-        "wizard/account_invoice_state_views.xml",
+        "wizard/account_move_state_views.xml",
     ]
 }
