2bd4dfd688ff68b11db55029628e99998ce75602
Odoo Aktiv <odoo@aktivsoftware.com>
2024-01-12 16:47:55 +0530

    [IMP] US 1: Solved issue of purchase order report layout.

---
+++ opt_purchases/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "OPT Purchases",
+    'summary': """Purchase Order Report Layout""",
+    'description': """User Story 1""",
+    'author': 'S4 Solutions, LLC',
+    'website': 'https://www.sfour.io/',
+    'category': 'purchase',
+    'version': '15.0.1.0.0',
+    'depends': ['purchase'],
+    "license": "AGPL-3",
+    "installable": True,
+    "application": True,
+    'data': [
+        'reports/purchase_reports.xml',
+    ],
+}
