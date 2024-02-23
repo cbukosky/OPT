94d8326befb8e37ad3efa5222ed5531e01aa5dce
Odoo Aktiv <odoo@aktivsoftware.com>
2024-02-23 14:40:11 +0530

    [IMP] US 2: Attachment Model Access

---
+++ opt_security/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "OPT Security",
+    "summary": """Attachments security""",
+    "description": """User Story 2""",
+    "author": "Polaris Integrators",
+    "website": "https://polarisintegrators.com/",
+    "version": "15.0.1.0.0",
+    "depends": ["product", "mrp_plm"],
+    "license": "AGPL-3",
+    "installable": True,
+    "application": True,
+    "data": [
+        "views/ir_attachment_views.xml",
+        "views/product_template_views.xml",
+    ],
+}
